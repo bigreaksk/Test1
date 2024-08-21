82b011065a90954d583ef79b7de4bdd54f8169b7
bigreaksk <40921558+bigreaksk@users.noreply.github.com>
2024-08-21 14:37:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ import streamlit as st
 import seaborn as sns
 df = sns.load_dataset('iris')
 df
+
+# create sidebar menu for user can select classifiers
+classifier = st.sidebar.selectbox('Classifier', ('KNN', 'SVM', 'Decision Tree', 'Random Forest', 'Neural Network'))
